b6988fb511b9920b121a41ccb41f8ce6c4a95a3c
zivivle <123868471+zivivle@users.noreply.github.com>
2025-09-14 14:34:01 +0900

    feat: blockchain

---
+++ blockchain/blockchain.go
@@ -0,0 +1,55 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	Data     string
+	Hash     string
+	PrevHash string
+}
+
+type blockchain struct {
+	blocks []*block
+}
+
+var b *blockchain
+
+func getLastHash() string {
+	blocks := GetBlockchain().blocks
+	totalLen := len(blocks)
+	if totalLen == 0 {
+		return ""
+	}
+	return blocks[totalLen-1].Hash
+}
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func GetBlockchain() *blockchain {
+	if b == nil {
+		b = &blockchain{}
+		b.AddBlock("Genesis Block")
+	}
+	return b
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zivivle/go/blockchain"
+)
+
+func main() {
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	blocks := chain.AllBlocks()
+	for _, block := range blocks {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("PrevHash: %s\n", block.PrevHash)
+	}
+}
