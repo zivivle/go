32d5247d4d26f07d9c289d809cc92963587b5ed9
zivivle <123868471+zivivle@users.noreply.github.com>
2025-09-20 17:04:47 +0900

    feat: flags setting

---
+++ cli/cli.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/zivivle/go/explorer"
+	"github.com/zivivle/go/rest"
+)
+
+func usage() {
+	fmt.Printf("Welcome to Z Coin\n\n")
+	fmt.Printf("Please use the following flags:\n\n")
+	fmt.Printf("-port=4000:		Set the PORT of the server\n")
+	fmt.Printf("-mode=rest:		Choose between 'html' and 'rest'\n\n")
+	// 프로그램을 종료시켜줌
+	// 0은 에러가 없다는 뜻이고
+	// 다른 숫자는 exit code를 보여줌
+	os.Exit(0)
+}
+
+func Start() {
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+}
--- main.go
+++ main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("Welcome to Z Coin\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer:	Start the HTML Explorer\n")
-	fmt.Printf("rest:		Start the REST API (recommended)\n\n")
-	// 프로그램을 종료시켜줌
-	// 0은 에러가 없다는 뜻이고
-	// 다른 숫자는 exit code를 보여줌
-	os.Exit(0)
-}
+import "github.com/zivivle/go/cli"
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-	portFlag := rest.Int("port", 4000, "Set the port of the server")
-
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Println(*portFlag)
-		fmt.Println("Start server")
-	}
+	cli.Start()
 }
